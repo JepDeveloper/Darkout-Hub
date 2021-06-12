89cc200f20dcaf949ccf7dd850dd13eae02a554e
PandaCyphy <72982537+PandaCyphy@users.noreply.github.com>
2021-06-12 20:11:25 +0530

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,1 @@
-if game.PlaceId == 286090429 then -- Ninja Legends 2
-    game:GetService("Players").LocalPlayer:Kick("Not for you") --keeping this open source cause this is an troll
-elseif game.PlaceId == 1962086868 then -- Tower Of Hell
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/PandaCyphy/Darkout-Hub/main/main/TowerOfHell.lua'))()
-end 
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PandaCyphy/Darkout-Hub/main/main/TowerOfHell.lua'))()
